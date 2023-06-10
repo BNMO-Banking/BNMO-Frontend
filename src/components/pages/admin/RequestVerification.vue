56e3e7a575d47f433a928422aceecef7982b4f06
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-06-10 12:07:20 +0700

    update request verification

--- src/components/pages/admin/RequestVerification.vue
+++ src/components/pages/admin/RequestVerification.vue
@@ -1,37 +1,15 @@
 <script setup lang="ts">
 import { ref, watch } from "vue";
-import axios from "axios";
-import router from "../../../router/router";
-import { useToast } from "vue-toastification";
 import RequestVerificationRow from "../../tablerow/RequestVerificationRow.vue";
-import RequestMoney from "../../../types/money-request";
+import { useRequestVerificationStore } from "../../../store/request-verification.store";
+import { storeToRefs } from "pinia";
 
-const toast = useToast();
-
-const datas = ref<RequestMoney[]>();
 const page = ref(1);
-const totalPage = ref(1);
-const emptyData = ref(false);
 
-const getPendingRequests = () => {
-    axios
-        .get(
-            `http://localhost:8080/admin/get-pending-request?page=${page.value}`,
-            {
-                withCredentials: true,
-            }
-        )
-        .then((response) => {
-            datas.value = response.data.data;
-            totalPage.value = response.data.metadata.last_page;
-            if (response.data.metadata.total === 0) {
-                emptyData.value = true;
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-};
+const store = useRequestVerificationStore()
+
+const { pendingRequests, pageMetadata, isLoadingPendingRequests, errorPendingRequests } = storeToRefs(store)
+store.getPendingRequests(page.value)
 
 const validateRequest = (id: number, isAccepted: boolean) => {
     const data = {
@@ -39,37 +17,15 @@ const validateRequest = (id: number, isAccepted: boolean) => {
         status: isAccepted ? "accepted" : "rejected",
     };
 
-    axios
-        .post("http://localhost:8080/admin/validate-request", data, {
-            withCredentials: true,
-        })
-        .then((response) => {
-            toast.success(response.data.message, {
-                timeout: 5000,
-            });
-            getPendingRequests();
-        })
-        .catch((err) => {
-            toast.error(err.response.data.error, {
-                timeout: 5000,
-            });
-            if (err.response.status === 400) {
-                getPendingRequests();
-            }
-        });
+    store.validateRequest(data)
 };
 
 watch(
-    [router.currentRoute, page],
+    page,
     () => {
-        getPendingRequests();
-    },
-    {
-        immediate: true,
+        store.getPendingRequests(page.value)
     }
 );
-
-watch(datas, () => {});
 </script>
 
 <template>
@@ -97,8 +53,8 @@ watch(datas, () => {});
             </thead>
             <tbody class="text-center">
                 <RequestVerificationRow
-                    v-if="!emptyData"
-                    v-for="(data, index) in datas"
+                    v-if="pendingRequests.length !== 0"
+                    v-for="(data, index) in pendingRequests"
                     :key="data.ID"
                     :data="data"
                     :index="index + 1 + (page - 1) * 5"
@@ -109,13 +65,23 @@ watch(datas, () => {});
         </table>
         <h3
             class="flex text-3xl text-main-red font-extrabold items-center justify-center w-full uppercase"
-            v-if="emptyData"
+            v-if="pendingRequests.length === 0"
         >
             No request to verify
         </h3>
+        <svg v-if="isLoadingPendingRequests" aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg>
+        <h3
+            class="flex text-3xl text-main-red font-extrabold items-center justify-center w-full uppercase"
+            v-if="errorPendingRequests"
+        >
+            {{ errorPendingRequests }}
+        </h3>
         <div
             class="flex justify-center items-center gap-x-16"
-            v-if="!emptyData"
+            v-if="pendingRequests.length !== 0"
         >
             <button
                 class="navbar-button bg-main-green border-main-green hover:scale-105 hover:text-white"
@@ -132,12 +98,12 @@ watch(datas, () => {});
                 Prev Page
             </button>
             <h4 class="text-xl font-extrabold uppercase text-center">
-                {{ page }} / {{ totalPage }}
+                {{ page }} / {{ pageMetadata.total }}
             </h4>
             <button
                 class="navbar-button bg-main-green border-main-green hover:scale-105 hover:text-white"
                 @click="page++"
-                v-if="page + 1 <= totalPage"
+                v-if="page + 1 <= pageMetadata.total"
             >
                 Next Page
             </button>
