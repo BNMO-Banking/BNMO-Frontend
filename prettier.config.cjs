40d0b3a4eed93fdcc5995e5bf177a49aaf46cc0f
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-06-21 15:46:16 +0700

    add prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+  bracketSpacing: true,
+  trailingComma: "none",
+  tabWidth: 4,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: true,
+  arrowParens: "always",
+  endOfLine: "auto"
+};
+
+module.exports = config;
