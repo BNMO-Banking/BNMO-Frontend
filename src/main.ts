275a77fe43ee4764e52a48b64e8e8a8b76d726b0
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-06-28 15:14:40 +0700

    feat: add new eslint, fix main typing, add env

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from "vue";
+import { createApp, type Component } from "vue";
 import { createPinia } from "pinia";
 import "./style.css";
 import axios from "axios";
@@ -8,12 +8,16 @@ import App from "./App.vue";
 import "vue-toastification/dist/index.css";
 
 const pinia = createPinia();
-axios.defaults.baseURL = import.meta.env.VITE_VUE_APP_BASE_URL
+axios.defaults.baseURL = import.meta.env.VITE_VUE_APP_BASE_URL;
 
-const token = localStorage.getItem('token')
+const token = localStorage.getItem("token");
 
 if (token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 }
 
-createApp(App).use(pinia).use(router).use(Toast, {timeout: 5000}).mount("#app");
+const app = createApp(App as Component);
+app.use(pinia);
+app.use(router);
+app.use(Toast, { timeout: 5000 });
+app.mount("#app");
