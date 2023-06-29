e441e713f7719cc78d6760a8c60a1d4a0b93ff5d
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-06-29 19:57:57 +0700

    feat: new checkbox input
    also make other inputs more flexible

---
+++ src/components/form/CheckboxInput.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps({
+    id: {
+        type: String,
+        required: true
+    },
+    required: {
+        type: Boolean
+    }
+});
+
+const emit = defineEmits<{ (event: "checked", payload: Event): void }>();
+</script>
+
+<template>
+    <div class="flex w-full items-center gap-x-4">
+        <input
+            class="w-4 h-4"
+            type="checkbox"
+            :id="id"
+            :required="required"
+            @change="emit('checked', $event)"
+        />
+        <label :for="id"><slot></slot></label>
+    </div>
+</template>
