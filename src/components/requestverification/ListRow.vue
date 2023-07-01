877bdc9804aabbdc93e6818db3ce5545451fc56f
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-07-01 11:09:07 +0700

    feat: remarks section and sending request

--- src/components/requestverification/ListRow.vue
+++ src/components/requestverification/ListRow.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
 import { RequestType } from "../../enum/reqtype.enum";
 import { Status } from "../../enum/status.enum";
 import RequestData from "../../types/model/request-data.type";
-defineProps({
+import { useRequestVerificationStore } from "../../store/request-verification.store";
+import { RejectRemarksReq } from "../../types/axios/request-verif.type";
+
+const props = defineProps({
     data: {
         type: Object as () => RequestData,
         required: true
@@ -10,7 +14,29 @@ defineProps({
     width: {
         type: Array<string>,
         required: true
+    },
+    checked: {
+        type: Boolean
     }
+});
+
+const remarks = ref({} as RejectRemarksReq);
+const error = ref("");
+const store = useRequestVerificationStore();
+const validateRequest = (id: string, isAccepted: boolean) => {
+    if (!isAccepted && remarks.value.remarks.length === 0) {
+        error.value = "Please fill the remarks section to reject";
+    } else {
+        store.postValidateRequest(
+            id,
+            isAccepted ? Status.ACCEPTED : Status.REJECTED,
+            remarks.value
+        );
+    }
+};
+
+onMounted(() => {
+    remarks.value.remarks = props.data.remarks;
 });
 </script>
 
@@ -23,7 +49,7 @@ defineProps({
                 : `bg-main-red/20 hover:bg-main-red/30 border-2 border-main-red`
         "
     >
-        <input :class="width[0]" class="w-4 h-4" type="checkbox" />
+        <input :class="width[0]" class="w-4 h-4" type="checkbox" :checked="checked" />
         <div :class="width[1]" class="flex items-center justify-center gap-x-2 p-4">
             <svg
                 v-if="data.request_type === RequestType.ADD"
@@ -144,40 +170,46 @@ defineProps({
             <div class="flex gap-x-2">
                 <p class="w-1/4">Remarks</p>
                 <p>:</p>
-                <div class="overflow-auto w-[70%] h-[4.5rem] max-h-[4.5rem]">
-                    {{ data.remarks || "No remarks" }}
-                </div>
+                <textarea
+                    v-model="remarks.remarks"
+                    rows="3"
+                    class="border border-black overflow-auto w-[70%] resize-none"
+                    :disabled="data.status !== Status.PENDING"
+                ></textarea>
             </div>
         </div>
-        <div :class="width[4]" class="flex justify-center gap-x-4">
-            <button class="action-yes">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    class="w-6 h-6"
-                >
-                    <path
-                        fill-rule="evenodd"
-                        d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
-                        clip-rule="evenodd"
-                    />
-                </svg>
-            </button>
-            <button class="action-no">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    class="w-6 h-6"
-                >
-                    <path
-                        fill-rule="evenodd"
-                        d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
-                        clip-rule="evenodd"
-                    />
-                </svg>
-            </button>
+        <div :class="width[4]" class="flex flex-col items-center justify-center gap-y-2">
+            <div class="flex gap-x-4">
+                <button class="action-yes" @click="validateRequest(data.id, true)">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        class="w-6 h-6"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z"
+                            clip-rule="evenodd"
+                        />
+                    </svg>
+                </button>
+                <button class="action-no" @click="validateRequest(data.id, false)">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        class="w-6 h-6"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+                            clip-rule="evenodd"
+                        />
+                    </svg>
+                </button>
+            </div>
+            <p class="text-center text-main-red">{{ error }}</p>
         </div>
     </div>
 </template>
--- src/types/axios/request-verif.type.ts
+++ src/types/axios/request-verif.type.ts
@@ -2,6 +2,10 @@ import { AxiosResponse } from "axios";
 import RequestData from "../model/request-data.type";
 import PageMetadata from "../model/page-metadata.type";
 
+export interface RejectRemarksReq {
+    remarks: string;
+}
+
 export interface RequestDataList extends AxiosResponse {
     data: RequestData[];
     metadata: PageMetadata;
