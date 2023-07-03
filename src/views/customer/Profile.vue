a8a3e3a44f40882f6e5c49e4383698fb0d55f638
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-07-03 21:32:30 +0700

    feat: editable profile picture

--- src/views/customer/Profile.vue
+++ src/views/customer/Profile.vue
@@ -3,6 +3,7 @@ import SpinnerLoading from "../../components/form/SpinnerLoading.vue";
 import BankCard from "../../components/profile/BankCard.vue";
 import CustomerAddress from "../../components/profile/CustomerAddress.vue";
 import ProfileHeader from "../../components/profile/ProfileHeader.vue";
+import ProfilePicture from "../../components/profile/ProfilePicture.vue";
 import { useAuthStore } from "../../store/auth.store";
 import { useProfileStore } from "../../store/profile.store";
 import { storeToRefs } from "pinia";
@@ -21,20 +22,7 @@ profileStore.getProfile(account.value.id);
     </div>
     <main v-else class="flex flex-col">
         <div class="flex px-64 py-8 gap-x-16 items-center">
-            <div class="flex items-center justify-center rounded-full p-8 bg-[#7a7979]">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="white"
-                    class="w-56 h-56"
-                >
-                    <path
-                        fill-rule="evenodd"
-                        d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-                        clip-rule="evenodd"
-                    />
-                </svg>
-            </div>
+            <ProfilePicture :profile-pic="profile.profile_pic_path" />
             <ProfileHeader
                 :account-type="profile.account_type"
                 :full-name="`${profile.first_name} ${profile.last_name}`"
