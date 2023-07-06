23bcea21c18aa1474b5ea9af5542ac70b50b1344
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-07-06 20:44:46 +0700

    feat: new months and years consts

--- src/options/months.options.ts
+++ src/options/months.options.ts
@@ -1,14 +1,34 @@
-export const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
-];
+const monthsMap = new Map([
+    [0, "Jan"],
+    [1, "Feb"],
+    [2, "Mar"],
+    [3, "Apr"],
+    [4, "May"],
+    [5, "Jun"],
+    [6, "Jul"],
+    [7, "Aug"],
+    [8, "Sep"],
+    [9, "Oct"],
+    [10, "Nov"],
+    [11, "Dec"]
+]);
+
+export const months = {
+    selected: monthsMap.get(new Date().getMonth()),
+    lists: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    multiselectLists: [
+        "All",
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+    ]
+};
